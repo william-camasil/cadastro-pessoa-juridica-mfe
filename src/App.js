364b925858c82b167de9655ab9592f87535d1fad
willliamcamasil <williamcamargosilva@gmail.com>
2026-01-08 07:12:23 -0300

    feat: adiciona configurações iniciais do projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="cad-pessoa-juridica">
+      <p className="cad-pessoa-juridica-p">Cadastro Pessoa Juridica - MFE</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.prod.js
@@ -0,0 +1,62 @@
+// header-app/webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// import ModuleFederationPlugin from webpack
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+// import dependencies from package.json, which includes react and react-dom
+const { dependencies } = require("./package.json");
+
+module.exports = {
+  entry: "./src/entry.js",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "cadastro_pessoa_juridica_mfe", // This application named 'cadastro_pessoa_juridica_mfe'
+      filename: "remoteEntry.js", // output a js file
+      exposes: {
+        // which exposes
+        "./CadPessoaJuridicaScreen": "./src/App", // a module 'CadPessoaJuridicaScreen' from './src/App'
+      },
+      shared: {
+        // and shared
+        ...dependencies, // some other dependencies
+        react: {
+          // react
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          // react-dom
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
